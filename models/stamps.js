446f6cdb01dd0ffeb197e66e161b06f0ce0d9aad
hyunhoh <skins0329@gmail.com>
2020-11-13 15:25:43 +0900

    [Server] / #7 / create tickets, stamps model and migrate

---
+++ models/stamps.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class stamps extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  stamps.init({
+    name: DataTypes.STRING,
+    number: DataTypes.STRING,
+    count: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'stamps',
+  });
+  return stamps;
+};
---
+++ models/tickets.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tickets extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  tickets.init({
+    name: DataTypes.STRING,
+    number: DataTypes.STRING,
+    count: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'tickets',
+  });
+  return tickets;
+};
