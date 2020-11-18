7fdc6fa92c202865109a0f8553b1e6b702865f67
hyunhoh <skins0329@gmail.com>
2020-11-19 04:33:29 +0900

    [Server] / #19 / feature: modified migration files

--- migrations/20201113062232-create-tickets.js
+++ migrations/20201113062232-create-tickets.js
@@ -1,33 +1,56 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('tickets', {
+    await queryInterface.createTable("tickets", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
-      name: {
-        type: Sequelize.STRING
+      title: {
+        type: Sequelize.STRING,
       },
-      number: {
-        type: Sequelize.STRING
+      curNum: {
+        type: Sequelize.INTEGER,
       },
-      count: {
-        type: Sequelize.INTEGER
+      maxNum: {
+        type: Sequelize.INTEGER,
+      },
+      is_used: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      start_date: {
+        type: Sequelize.STRING,
+      },
+      end_date: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "users",
+          },
+          key: "id",
+        },
+      },
+      tickets_group_id: {
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('tickets');
-  }
+    await queryInterface.dropTable("tickets");
+  },
 };
--- migrations/20201113062244-create-stamps.js
+++ migrations/20201113062244-create-stamps.js
@@ -1,33 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('stamps', {
+    await queryInterface.createTable("stamps", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       number: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       count: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "users",
+          },
+          key: "id",
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('stamps');
-  }
+    await queryInterface.dropTable("stamps");
+  },
 };
