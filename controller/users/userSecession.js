677055017942f3aae8a07e424924e9e8a42a4bba
jesussangho <jesussangho@github.com>
2020-11-19 15:32:58 +0900

    [Server] / #17 / userSecession.js

--- controller/users/userSecession.js
+++ controller/users/userSecession.js
@@ -2,21 +2,25 @@ const { users } = require("../../models");
 
 module.exports = {
   delete: async (req, res) => {
-    const { email, password } = req.body;
-    const result = await users.destroy({
-      where: {
-        email,
-        password
-      }
-    })
-    if (result) {
-      result.destroy({
-        truncate: true
+    const { user_id } = req.params;
+    try {
+      const result = await users.destroy({
+        where: {
+          id: user_id
+        },
       })
-        res.status(200).send(result)
-        res.redirect("/");
-    } else {
-        res.status(500).send('Failure to withdraw')
+      if (result) {
+        await req.session.destroy((err) => {
+          if (err) {
+            res.status(404).send("invalid user");
+          } else {
+            res.status(200).send("Withdrawal completed");
+            res.redirect("/");
+          }
+        });
+      }
+    } catch(err) {
+      res.status(500).send('err')
     }
   },
 };
